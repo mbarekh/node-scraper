f8da78f21d353433f0382c66bbfc228adeb4584d
Hazem Mbarek <hazem.mbarek@hotmail.com>
2026-08-10 16:01:00 +0200

    Fix company id

--- scraper/utils/domain-utils.ts
+++ scraper/utils/domain-utils.ts
@@ -18,7 +18,7 @@ export const getPathName = (url: string): string => {
   try {
     const fullUrl = new URL(url);
     return fullUrl.pathname;
-  } catch (_error) {
+  } catch {
     return url;
   }
 };
@@ -34,7 +34,13 @@ export const getCompanyName = (url: string): string => {
   return hostname.split(".")?.[0] ?? "";
 };
 
-export const toAbsoluteUrl = ({ url, href }: { url: string; href: string }): string => {
+export const toAbsoluteUrl = ({
+  url,
+  href,
+}: {
+  url: string;
+  href: string;
+}): string => {
   if (!href) {
     return url;
   }
@@ -44,15 +50,31 @@ export const toAbsoluteUrl = ({ url, href }: { url: string; href: string }): str
   return new URL(href, url).href;
 };
 
-export const extractCompanyName = ({ url, regexes }: { url: string; regexes: RegExp[] }): string => {
-  const companyName = regexes.map((r) => url.match(r)?.[1]).find(Boolean) ?? null;
+export const extractCompanyName = ({
+  url,
+  regexes,
+}: {
+  url: string;
+  regexes: RegExp[];
+}): string => {
+  const companyName =
+    regexes.map((r) => url.match(r)?.[1]).find(Boolean) ?? null;
   if (!companyName) {
-    throw Error(`[${extractCompanyName.name}]: URL ${url} does not match expected pattern`);
+    throw Error(
+      `[${extractCompanyName.name}]: URL ${url} does not match expected pattern`,
+    );
   }
   return companyName;
 };
 
-export const getUrlWithParams = ({ url, params }: { url: string; params?: Record<string, any> }): string => {
+export const getUrlWithParams = ({
+  url,
+  params,
+}: {
+  url: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  params?: Record<string, any>;
+}): string => {
   if (!/^https:\/\//i.test(url)) {
     return "";
   }
